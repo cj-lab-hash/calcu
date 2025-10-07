583f2a5a66fd12d39456f22a88079c41731dd0af
cj-lab-hash <condadacj@gmail.com>
2025-10-08 05:55:39 +0800

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -9,7 +9,7 @@ function calc() {
     let yield= (bin1/ttested) * 100;
     var nbin1 = (ttested * syl1) + 1;
     let ndbin1 = nbin1 - bin1;
-    let uryield = (nbin1/bin8)*100;
+    let uryield = (ndbin1/bin8) * 100;
     
 
     var i = 0;
@@ -44,7 +44,7 @@ document.getElementById("ttested").innerHTML = "Total Device = " + ttested;
 document.getElementById("yield").innerHTML = "First Pass Yield = " + yield.toFixed(2) + "%";
 document.getElementById("nbin1").innerHTML = "Total Bin 1 needed to hit SYL = " + nbin1.toFixed(0);
 document.getElementById("ndbin1").innerHTML = "Needed Bin 1 during URE = " + ndbin1.toFixed(0);
-document.getelementbyid("uryield").innerhtml = "Yield during URE = " + uryield.tofixed(0);
+document.getElementById("uryield").innerHTML = "Yield during URE = " + uryield.tofixed(2) + "%";
 
     
   }
@@ -55,6 +55,7 @@ function clr() {
     document.getElementById("syl").value = '';
     window.location.reload();
 }
+
 
 
 
