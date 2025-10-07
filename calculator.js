573d2d02c7087e3f79733e7380908ae277c6c34f
cj-lab-hash <condadacj@gmail.com>
2025-10-08 07:46:32 +0800

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,63 +1,44 @@
-
 function calc() {
-    var bin1 = document.getElementById("bin1").valueAsNumber;
-    var bin8 = document.getElementById("bin8").valueAsNumber;
-    var syl = document.getElementById("syl").valueAsNumber;
-    
-    let syl1 = syl/100;
-    let ttested = bin1 + bin8;
-    let yield= (bin1/ttested) * 100;
-    var nbin1 = (ttested * syl1) + 1;
-    let ndbin1 = nbin1 - bin1;
-    let uryield = (ndbin1/bin8) * 100;
-    
+  const bin1 = document.getElementById("bin1").valueAsNumber;
+  const bin8 = document.getElementById("bin8").valueAsNumber;
+  const syl = document.getElementById("syl").valueAsNumber;
 
-    var i = 0;
-        if (i == 0) {
-    i = 1;
-    var elem = document.getElementById("myBar");
-    var width = 1;
-    var id = setInterval(frame, 25);
-function frame() {
-      if (width >= 100) {
-        clearInterval(id);
-        i = 0;
-      } else {
-        width++;
-        elem.style.width = width + "%";
-      }
+  if (isNaN(bin1) || isNaN(bin8) || isNaN(syl)) {
+    alert("Please enter valid numbers for all fields!");
+    return;
+  }
+
+  const syl1 = syl / 100;
+  const ttested = bin1 + bin8;
+  const fpYield = (bin1 / ttested) * 100;
+  const nbin1 = (ttested * syl1) + 1;
+  const ndbin1 = nbin1 - bin1;
+  const urYield = (ndbin1 / bin8) * 100;
+
+  // Animate progress bar
+  const elem = document.getElementById("myBar");
+  let width = 1;
+  const id = setInterval(() => {
+    if (width >= 100) {
+      clearInterval(id);
+      showResults();
+    } else {
+      width++;
+      elem.style.width = width + "%";
     }
-  }
+  }, 25);
 
-
-
-const myTimeOut = setTimeout(final, 2500);
-
-
-    
- 
-
-
-function final() {
-
-document.getElementById("ttested").innerHTML = "Total Device = " + ttested;
-document.getElementById("yield").innerHTML = "First Pass Yield = " + yield.toFixed(2) + "%";
-document.getElementById("nbin1").innerHTML = "Total Bin 1 needed to hit SYL = " + nbin1.toFixed(0);
-document.getElementById("ndbin1").innerHTML = "Needed Bin 1 during URE = " + ndbin1.toFixed(0);
-document.getElementById("uryield").innerHTML = "Yield during URE = " + uryield.tofixed(2) + "%";
-
-    
+  function showResults() {
+    document.getElementById("ttested").innerHTML = "Total Device = " + ttested;
+    document.getElementById("yield").innerHTML = "First Pass Yield = " + fpYield.toFixed(2) + "%";
+    document.getElementById("nbin1").innerHTML = "Total Bin 1 needed to hit SYL = " + nbin1.toFixed(0);
+    document.getElementById("ndbin1").innerHTML = "Needed Bin 1 during URE = " + ndbin1.toFixed(0);
+    document.getElementById("uryield").innerHTML = "Yield during URE = " + urYield.toFixed(2) + "%";
   }
 }
+
 function clr() {
-    document.getElementById("bin1").value = '';
-    document.getElementById("bin8").value = '';
-    document.getElementById("syl").value = '';
-    window.location.reload();
+  ["bin1", "bin8", "syl"].forEach(id => document.getElementById(id).value = '');
+  ["ttested", "yield", "nbin1", "ndbin1", "uryield"].forEach(id => document.getElementById(id).innerHTML = '');
+  document.getElementById("myBar").style.width = "0%";
 }
-
-
-
-
-
-
